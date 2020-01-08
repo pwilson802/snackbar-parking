063936c6cc84a6e6f2e55a86a86601769bd991ea
Paul Wilson <pwilson802@gmail.com>
2020-01-08 21:59:36 +1100

    changed debug to environment variable

--- snacksite/settings.py
+++ snacksite/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's6oe+b#(ly@9(@fm$juo*8c!xvotx-9ro187ep1@jbzq=n$&^c'
+# SECRET_KEY = 's6oe+b#(ly@9(@fm$juo*8c!xvotx-9ro187ep1@jbzq=n$&^c'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG_VALUE')
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['snackbarparking-prd.herokuapp.com', 'localhost', '127.0.0.1']
 
